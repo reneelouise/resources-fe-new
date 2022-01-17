f3d3a0363c07a4513e8e4a0cafdab88ae6eb635b
Ed Halliwell <edhalliwell@hotmail.com>
2022-01-17 17:40:05 +0000

    More test steps

--- cypress/integration/0-bibliotech/AddToStudyList.spec.js
+++ cypress/integration/0-bibliotech/AddToStudyList.spec.js
@@ -12,11 +12,14 @@ describe("Add a resource to study list, then remove it", () => {
     cy.get(".user-selected-login-btn").click(); //you can click the next login button when a user is selected from the dropdown
     cy.get(".logged-user-text-and-logout"); //you can see text that states a user is logged in
     cy.get(".links").should("have.length", 3); // three navlinks are showing
-    cy.get(".Resource-Card").first().get(".add_to_study_list_button").click();
-    cy.get(".Resource-Card")
-      .first()
-      .get(".remove_from_study_list_button")
-      .click();
+    cy.get(".Resource-Card").first();
+    cy.get(".add_to_study_list_button").first();
+    cy.get(".remove_from_study_list_button").first();
+
+    // cy.get(".Resource-Card")
+    //   .first()
+    //   .get(".remove_from_study_list_button")
+    //   .click();
     cy.get(".logged-user-text-and-logout").children(".logout-btn").click(); //a logout button can be clicked
     cy.get(".links").should("have.length", 1); // one navlink is showing again when user is logged out
   });
