eeb8cfdb5a34463e4a3ac06b6f7ccd8fff0e5ab7
Ed Halliwell <edhalliwell@hotmail.com>
2022-01-17 16:59:20 +0000

    Move files to folder, start on addtostudylist test file

---
+++ cypress/integration/0-bibliotech/AddToStudyList.spec.js
@@ -0,0 +1,23 @@
+describe("Add a resource to study list, then remove it", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/"); //localhost:3000 works
+  });
+  it("Logs a user in", () => {
+    cy.get(".Login-Button").click(); //you can click the login button
+    cy.get(".User-Dropdown-Menu").click(); // a user dropdown menu is showing and can be clicked
+    cy.get(".Users-In-Dropdown")
+      .first()
+      .should("have.text", "Ada Lovelace")
+      .click();
+    cy.get(".user-selected-login-btn").click(); //you can click the next login button when a user is selected from the dropdown
+    cy.get(".logged-user-text-and-logout"); //you can see text that states a user is logged in
+    cy.get(".links").should("have.length", 3); // three navlinks are showing
+    cy.get(".Resource-Card").first().get(".add_to_study_list_button").click();
+    cy.get(".Resource-Card")
+      .first()
+      .get(".remove_from_study_list_button")
+      .click();
+    cy.get(".logged-user-text-and-logout").children(".logout-btn").click(); //a logout button can be clicked
+    cy.get(".links").should("have.length", 1); // one navlink is showing again when user is logged out
+  });
+});
