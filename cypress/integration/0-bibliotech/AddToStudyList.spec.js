48a00f45aa8502fb4b51c76c01258fcab2d85cec
Ed Halliwell <edhalliwell@hotmail.com>
2022-01-18 10:09:44 +0000

    Finish test for add to and remove from study list flow

--- cypress/integration/0-bibliotech/AddToStudyList.spec.js
+++ cypress/integration/0-bibliotech/AddToStudyList.spec.js
@@ -13,13 +13,9 @@ describe("Add a resource to study list, then remove it", () => {
     cy.get(".logged-user-text-and-logout"); //you can see text that states a user is logged in
     cy.get(".links").should("have.length", 3); // three navlinks are showing
     cy.get(".Resource-Card").first();
+    cy.get(".add_to_study_list_button").first().click().wait(3000);
+    cy.get(".remove_from_study_list_button").first().click().wait(3000);
     cy.get(".add_to_study_list_button").first();
-    cy.get(".remove_from_study_list_button").first();
-
-    // cy.get(".Resource-Card")
-    //   .first()
-    //   .get(".remove_from_study_list_button")
-    //   .click();
     cy.get(".logged-user-text-and-logout").children(".logout-btn").click(); //a logout button can be clicked
     cy.get(".links").should("have.length", 1); // one navlink is showing again when user is logged out
   });
