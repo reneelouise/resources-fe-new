c44b751889cadcfd4a9661fec2dc49bc45d735fb
Renee-Louise Nzegbulem <renee@Renee-Louises-MacBook-Pro.local>
2022-01-19 12:19:01 +0000

    latest commit with cypress update

--- src/components/PageCount.tsx
+++ src/components/PageCount.tsx
@@ -1,18 +1,15 @@
 import Box from "@mui/material/Box";
 import Pagination from "@mui/material/Pagination";
 
-
 export default function PageCount(): JSX.Element {
-
-
-   return (
-      <Box sx={{
-         minWidth: "300px",
-         display: { xs: "flex" },
-      }}>
-         <Pagination count={10} variant="outlined" shape="rounded" />
-      </Box>
-
-
-   )
+  return (
+    <Box
+      sx={{
+        minWidth: "300px",
+        display: { xs: "flex" },
+      }}
+    >
+      <Pagination count={10} variant="outlined" shape="rounded" />
+    </Box>
+  );
 }
